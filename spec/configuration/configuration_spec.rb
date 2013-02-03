0d0534bf453479f6b65e2d43ca67b65b112b18bb
Declan McGrath <declanmg@gmail.com>
2013-02-03 14:21:32 +0000

    Extending the configuration model spec.

--- spec/configuration/configuration_spec.rb
+++ spec/configuration/configuration_spec.rb
@@ -7,14 +7,49 @@ describe Configuration do
   it { should belong_to(:application) }
   it { should belong_to(:domain) }
 
-  context 'creation' do
+  context 'when creating a channel configuration' do
     let(:channel) { Channel.create name: 'Channel' }
-    before(:each) do
-      @configuration = Configuration.create name: 'some_config_key',
-        value: { :age => 34, :name => "Sarah" }, channel: channel
+    subject(:configuration) { Configuration.create name: 'data_config', channel: channel,
+        value: { :max_shards => 200, :shard_size => 10000 }}
+    it {should be_saved}
+    it "should have the channel" do
+      configuration.reload.channel.should == channel
     end
-    it 'should be persisted' do @configuration.should be_saved end
+    it "should not be returned by Configuration.root" do
+      Configuration.root.should_not include(configuration)
+    end
   end
 
+  context 'when creating a application configuration' do
+    let(:channel) { Channel.create name: 'Channel' }
+    let(:application) { Application.create name: 'Application', channel: channel }
+    subject(:configuration) { Configuration.create name: 'app_config', application: application,
+        value: { :num_instances => 3}}
+    it {should be_saved}
+    it "should not be returned by Configuration.root" do
+      Configuration.root.should_not include(configuration)
+    end
+  end
+
+  context 'when creating a domain configuration' do
+    let(:channel) { Channel.create name: 'Channel' }
+    let(:application) { Application.create name: 'Application', channel: channel }
+    let(:domain) { Domain.create identifier: 'www.rubyjobs.ie', application: application }
+    subject(:configuration) { Configuration.create name: 'domain_config', domain: domain,
+        value: { :max_users => 200, :name_format => "<firstname> <surname>" }}
+    it {should be_saved}
+    it "should not be returned by Configuration.root" do
+      Configuration.root.should_not include(configuration)
+    end
+  end
+
+  context 'when creating a root configuration' do
+    subject(:configuration) { Configuration.create name: 'a_root_config',
+        value: { :country => 'IE', :city => "DUB" }}
+    it {should be_saved}
+    it "should not be returned by Configuration.root" do
+      Configuration.root.should include(configuration)
+    end
+  end
 end
 
